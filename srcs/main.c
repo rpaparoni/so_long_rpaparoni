ac885c5a9fcb2a58bd96ea611e7ec6c0c2e547a3
rpaparon <paparoniricardo@gmail.com>
2025-01-21 20:01:48 +0100

    test img

--- srcs/main.c
+++ srcs/main.c
@@ -6,18 +6,38 @@
 /*   By: rpaparon <rpaparon@student.42madrid.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 20:04:58 by rpaparon          #+#    #+#             */
-/*   Updated: 2025/01/21 18:56:24 by rpaparon         ###   ########.fr       */
+/*   Updated: 2025/01/21 19:59:50 by rpaparon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../mlx/mlx.h"
+#include <stdlib.h>
 
-int	main(void)
+int close_window(int keycode, void *param)
 {
-	void	*mlx;
-	void	*mlx_win;
+    if (keycode == 65307)
+    {
+        mlx_destroy_window(param, param);
+        exit(0);
+    }
+    return (0);
+}
+int main(void)
+{
+    void	*mlx;
+    void	*mlx_win;
+	void	*img;
+	int	width;
+    int	height;
+	char	*relative_path = "../textures/open.xpm";
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "so_long");
-	mlx_loop(mlx);
+    mlx = mlx_init();
+    mlx_win = mlx_new_window(mlx, 1920, 1080, "so_long");
+	img = mlx_xpm_file_to_image(mlx, relative_path, &width, &height);
+	mlx_put_image_to_window(mlx, mlx_win, img, 500, 500);
+	mlx_key_hook(mlx_win, close_window, mlx_win);
+
+    mlx_loop(mlx);
+
+    return 0;
 }
