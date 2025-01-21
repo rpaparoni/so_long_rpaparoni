ee0628af64bd5bdb3e128d59a22b9e1d7889df6c
rpaparon <paparoniricardo@gmail.com>
2025-01-21 18:57:51 +0100

    new window & minilib

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,18 @@
 /*   By: rpaparon <rpaparon@student.42madrid.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 20:04:58 by rpaparon          #+#    #+#             */
-/*   Updated: 2025/01/15 20:26:05 by rpaparon         ###   ########.fr       */
+/*   Updated: 2025/01/21 18:56:24 by rpaparon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main()
+#include "../mlx/mlx.h"
+
+int	main(void)
 {
-	test
+	void	*mlx;
+	void	*mlx_win;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "so_long");
+	mlx_loop(mlx);
 }
