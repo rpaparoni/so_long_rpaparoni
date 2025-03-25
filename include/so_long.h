5b66859b9b47a7db33de19111a6e1e322eaa0280
Ricardo <paparoniricardo@gmail.com>
2025-03-25 14:44:07 +0100

    new list

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: rpaparon <rpaparon@student.42madrid.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 20:05:31 by rpaparon          #+#    #+#             */
-/*   Updated: 2025/03/06 10:47:18 by rpaparon         ###   ########.fr       */
+/*   Updated: 2025/03/25 14:42:55 by rpaparon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,11 @@ typedef struct s_game
 	void	*mlx;
 	void	*win;
 	void	*img;
+	void	*player;
+	void	*wall;
+	void	*floor;
+	void	*collectible;
+	void	*exit;
 }	t_game;
 
 int	close_window(int keycode, t_game *game);
--- sources/render.c
+++ sources/render.c
@@ -6,9 +6,24 @@
 /*   By: rpaparon <rpaparon@student.42madrid.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 15:15:33 by rpaparon          #+#    #+#             */
-/*   Updated: 2025/03/20 16:57:27 by rpaparon         ###   ########.fr       */
+/*   Updated: 2025/03/25 14:41:13 by rpaparon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minilibx-linux/mlx.h"
 #include "../include/so_long.h"
+
+char    *get_texture(t_game *game, char c)
+{
+    if (c == '1')
+        return (game->textures.wall);
+    else if (c == '0')
+        return (game->textures.floor);
+    else if (c == 'C')
+        return (game->textures.collectible);
+    else if (c == 'E')
+        return (game->textures.exit);
+    else if (c == 'P')
+        return (game->textures.player);
+    return (NULL);
+}
