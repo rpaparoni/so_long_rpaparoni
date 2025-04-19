e436aa9dd449dcb86e9834fab4ff29b2f3841967
Ricardo <paparoniricardo@gmail.com>
2025-04-19 17:32:45 +0200

    imprime hell yeah

--- sources/hooks.c
+++ sources/hooks.c
@@ -6,35 +6,35 @@
 /*   By: rpaparon <rpaparon@student.42madrid.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 15:15:22 by rpaparon          #+#    #+#             */
-/*   Updated: 2025/04/17 15:22:38 by rpaparon         ###   ########.fr       */
+/*   Updated: 2025/04/19 17:19:41 by rpaparon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minilibx-linux/mlx.h"
 #include "../include/so_long.h"
 
-void    s_key(t_game *game)
-{
-    print_moves(game);
-    game->pos_y += 64;
-    mlx_put_image_to_window(game->mlx, game->win, game->player,
-        game->pos_x, game->pos_y);
-}
-void    a_key(t_game *game)
+void    key_down(t_game *game)
 {
     print_moves(game);
     game->pos_x -= 64;
     mlx_put_image_to_window(game->mlx, game->win, game->player,
         game->pos_x, game->pos_y);
 }
-void    d_key(t_game *game)
+void    key_left(t_game *game)
 {
     print_moves(game);
     game->pos_x += 64;
     mlx_put_image_to_window(game->mlx, game->win, game->player,
         game->pos_x, game->pos_y);
 }
-void    w_key(t_game *game)
+void    key_right(t_game *game)
+{
+    print_moves(game);
+    game->pos_x += 64;
+    mlx_put_image_to_window(game->mlx, game->win, game->player,
+        game->pos_x, game->pos_y);
+}
+void    key_up(t_game *game)
 {
     print_moves(game);
     game->pos_y -= 64;
@@ -45,13 +45,13 @@ void    w_key(t_game *game)
 int    hooks(int keycode, t_game *game)
 {
     if (keycode == 65362 || keycode == 119)
-        w_key(game);
+        key_up(game);
     else if (keycode == 65364 || keycode == 115)
-        a_key(game);
+        key_left(game);
     else if (keycode == 65361 || keycode == 100)
-        s_key(game);
+        key_down(game);
     else if (keycode == 65363 || keycode == 97)
-        d_key(game);
+        key_right(game);
     else if (keycode == 65307)
         ft_kill("Game closed", game);
     return (0);
